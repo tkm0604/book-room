e9ecc9e79d72d9ed3a0debd5ccdaf6db27f7a855
yamaguchi.takumi <gu.jp06042gmail.com>
2024-11-26 18:18:23 +0900

    Xアカウント作成時のバグ修正

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -16,11 +16,15 @@ class EnsureEmailIsVerified
      */
     public function handle(Request $request, Closure $next): Response
     {
+
+        $user = Auth::user();
         // ログイン済みで、メールが未認証の場合はリダイレクト
-        if(Auth::check() && is_null(Auth::user()->email_verified_at)){
+        // if(Auth::check() && is_null(Auth::user()->email_verified_at)
+        if($user && is_null($user->email_verified_at) &&  is_null($user->twitter_token))
+        {
             return redirect('/verify-email');
         }
-        
+
         return $next($request);
     }
 }
