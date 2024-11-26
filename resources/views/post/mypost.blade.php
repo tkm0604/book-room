d78004da9fe5fd2bca0e5a739c926b01388a7fb5
yamaguchi.takumi <gu.jp06042gmail.com>
2024-11-26 14:16:49 +0900

    レイアウト崩れ修正

--- resources/views/post/mypost.blade.php
+++ resources/views/post/mypost.blade.php
@@ -17,48 +17,45 @@
                 <span class="font-bold text-sky-700">投稿の作成にはアカウント登録が必須です。</span>
             </p>
         @endif --}}
-            @if(count($posts) == 0)
-            <p class="mt-4">
-                あなたはまだ投稿していません。
-            </p>
+            @if (count($posts) == 0)
+                <p class="mt-4">
+                    あなたはまだ投稿していません。
+                </p>
             @else
-            @foreach ($posts as $post )
-            <div class="mt-4">
-                <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
-                    <div class="mt-4">
-                        <div class="bg-white w-full  rounded-2xl px-10 pt-2 pb-8 shadow-lg hover:shadow-2xl transition duration-500">
-                            <div class="mt-4">
-                                <div class="flex">
-                                    <div class="rounded-full w-12 h-12">
-                                        {{-- アバター表示 --}}
-                                        <img src="{{asset($post->user->avatar??'user_default.jpg')}}">
-                                    </div>
-                                    <h1 class="text-lg text-indigo-700 font-semibold hover:underline cursor-pointer float-left pt-4">
-                                        <a href="{{route('post.show', $post)}}">{{ $post->title }}</a>
-                                    </h1>
+                @foreach ($posts as $post)
+                <div class="mt-4">
+                    <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
+                        <div class="mt-4">
+                            <div class="flex">
+                                <div class="rounded-full w-12 h-12">
+                                    {{-- アバター表示 --}}
+                                    <img src="{{ isset($post->user) && $post->user->avatar !== 'user_default.jpg' ? asset($post->user->avatar) : asset('storage/avatar/user_default.jpg') }}">
                                 </div>
-                        <hr class="w-full">
-                        <p class="mt-4 text-gray-600 py-4 break-words">{{ Str::limit($post->body,100,'...') }}</p>
-                        <div class="text-sm font-semibold flex flex-row-reverse">
-                            <p>{{ $post->user->name }} • {{ $post->created_at->diffForHumans() }}</p>
+                                <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer float-left pt-4">
+                                    <a href="{{route('post.show', $post)}}">{{ $post->title }}</a>
+                                </h1>
+                            </div>
+                            <hr class="w-full">
+                            <p class="mt-4 text-gray-600 py-4 break-words">{{ Str::limit($post->body,100,'...') }}</p>
+                            <div class="text-sm font-semibold flex flex-row-reverse">
+                                <p>{{ $post->user->name }} • {{ $post->created_at->diffForHumans() }}</p>
+                            </div>
+                            <hr class="w-full mb-2">
+                            @if($post->comments->count())
+                            <span class="badge">
+                                返信{{ $post->comments->count() }}件
+                            </span>
+                            @else
+                            <span>コメントはまだありません。</span>
+                            @endif
+                            <a href="{{route('post.show', $post)}}" style="color:white;">
+                                <x-primary-button class="float-right">コメントする</x-primary-button>
+                         </a>
                         </div>
-                        <hr class="w-full mb-2">
-                        @if($post->comments->count())
-                        <span class="badge">
-                            返信{{ $post->comments->count() }}件
-                        </span>
-                        @else
-                        <span>コメントはまだありません。</span>
-                        @endif
-                        <a href="{{route('post.show', $post)}}" style="color:white;">
-                            <x-primary-button class="float-right">コメントする</x-primary-button>
-                     </a>
                     </div>
                 </div>
-            </div>
         @endforeach
         @endif
-    </div>
 
     </div>
 </x-app-layout>
